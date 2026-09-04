b1c06ac5191c0ab3dff99106058913b23a5857af
lakshminaidu093@gmail.com <lakshminaidu093@gmail.com>
2026-09-04 17:35:11 +0530

    allowed origins

--- myproject/settings.py
+++ myproject/settings.py
@@ -128,4 +128,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
-CORS_ALLOWED_ORIGINS=[ 'http://localhost:5173/']
+CORS_ALLOWED_ORIGINS=[ 'http://localhost:5173/','.render.com']
+# EVERIKI PAMPINCHALI
